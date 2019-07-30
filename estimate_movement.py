5f9230718501722d7ffacf620e5b269ea09cde62
qwembe <qwembe@gmail.com>
2019-07-30 22:02:37 +0300

    commited my previous work. instead is used opencv function. sad but it works better

--- estimate_movement.py
+++ estimate_movement.py
@@ -5,6 +5,7 @@ from tracking import get_solution, get_scale_ratio
 from triangulation import rotate_cords
 from numpy.linalg import inv
 import numpy as np
+import cv2
 
 
 #
@@ -12,31 +13,47 @@ import numpy as np
 #
 
 def process_vo(p0, p1, p2):
-    image0 = np.array([[p[0]/1000, p[1]/1000, 1.0] for p in p0])
-    image1 = np.array([[p[0]/1000, p[1]/1000, 1.0] for p in p1])
-    image2 = np.array([[p[0]/1000, p[1]/1000, 1.0] for p in p2])
+    image0 = np.array([[p[0] / 1000, p[1] / 1000, 1.0] for p in p0])
+    image1 = np.array([[p[0] / 1000, p[1] / 1000, 1.0] for p in p1])
+    image2 = np.array([[p[0] / 1000, p[1] / 1000, 1.0] for p in p2])
     # print(image0)
     # print(image1)
     # print(image2)
     # Making essential matrix
     # print("Make essential matrix from camera1 and camera2")
-    E1 = createEssenceMatrix(image0, image1)
-    # print("Make essential matrix from camera2 and camera3")
-    E2 = createEssenceMatrix(image1, image2)
+    # E1 = createEssenceMatrix(image0, image1)
+    # # print("Make essential matrix from camera2 and camera3")
+    # E2 = createEssenceMatrix(image1, image2)
 
-    four_solution_A = e_decomposition(E1)
-    four_solution_B = e_decomposition(E2)
+    E1 = cv2.findEssentialMat(p0, p1)[0].T
+    E2 = cv2.findEssentialMat(p1, p2)[0].T
 
-    A, Rt1, T1 = get_solution(four_solution_A, image0, image1)
-    B, Rt2, T2 = get_solution(four_solution_B, image1, image2)
+
+
+    # four_solution_A = e_decomposition(E1)
+    # four_solution_B = e_decomposition(E2)
+
+    # four_solution_A = cv2.recoverPose(E1,p0,p1)
+    # four_solution_B = cv2.recoverPose(E2,p1,p2)
+
+    tr, Rt1, T1, A = cv2.recoverPose(E1, p0, p1)
+    tr, Rt2, T2, B = cv2.recoverPose(E2, p1, p2)
+
+    # print(four_solution_A[4])
+
+
+    # exit(0)
+    # A, Rt1, T1 = get_solution(four_solution_A, image0, image1)
+    # B, Rt2, T2 = get_solution(four_solution_B, image1, image2)
 
     mcam1 = Camera(offset=T1)
     mcam2 = Camera(offset=T1 + inv(Rt1) @ T2)
     mcam1.set_NewBasis(Rt1)
     mcam2.set_NewBasis(Rt1 @ Rt2)
-    B = np.array(rotate_cords(mcam1, B))
+    # B = np.array(rotate_cords(mcam1, B))
 
-    scale = get_scale_ratio(A,B)
+    # scale = get_scale_ratio(A, B)
+    scale = 1
     print(scale)
 
     # make visual
@@ -44,24 +61,24 @@ def process_vo(p0, p1, p2):
     true_image2 = rotate_cords(mcam1, image1)
     true_image3 = rotate_cords(mcam2, image2)
 
-    mplt = mPlot()
-    mplt.show_camera_basis(Camera())
-    mplt.show_camera_basis(mcam1)
-    mplt.show_camera_basis(mcam2)
-
-    for i in range(0, len(image0)):
-        mplt.addVector(A[i], Camera().getOffset(), vectype="dashed")
-        mplt.addVector(A[i], mcam1.getOffset(), vectype="dashed")
-
-    for i in range(0, len(image1)):
-        mplt.addVector(B[i], mcam1.getOffset(), vectype="dashed")
-        mplt.addVector(B[i], mcam2.getOffset(), vectype="dashed", color="b")
-
-    mplt.addPoints(A, param='r')
-    mplt.addPoints(B, param='b')
-
-    mplt.addPoints(true_image1, param='g')
-    mplt.addPoints(true_image2, param='b')
-    mplt.addPoints(true_image3, param='c')
-    mplt.show(-50,50,-50,50,-50,50)
-    return True, T1, scale
+    # mplt = mPlot()
+    # mplt.show_camera_basis(Camera())
+    # mplt.show_camera_basis(mcam1)
+    # mplt.show_camera_basis(mcam2)
+    #
+    # for i in range(0, len(image0)):
+    #     mplt.addVector(A[i], Camera().getOffset(), vectype="dashed")
+    #     mplt.addVector(A[i], mcam1.getOffset(), vectype="dashed")
+    #
+    # for i in range(0, len(image1)):
+    #     mplt.addVector(B[i], mcam1.getOffset(), vectype="dashed")
+    #     mplt.addVector(B[i], mcam2.getOffset(), vectype="dashed", color="b")
+    #
+    # mplt.addPoints(A, param='r')
+    # mplt.addPoints(B, param='b')
+    #
+    # mplt.addPoints(true_image1, param='g')
+    # mplt.addPoints(true_image2, param='b')
+    # mplt.addPoints(true_image3, param='c')
+    # mplt.show(-50,50,-50,50,-50,50)
+    return True, np.array(T1), scale
--- index.py
+++ index.py
@@ -7,7 +7,7 @@ lk_params = dict(winSize=(19, 19),
                  maxLevel=2,
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-feature_params = dict(maxCorners=1000,
+feature_params = dict(maxCorners=1500,
                       qualityLevel=0.01,
                       minDistance=8,
                       blockSize=19)
@@ -28,6 +28,8 @@ green = (0, 255, 0)
 red = (0, 0, 255)
 
 currentFrame = 0
+
+
 def read():
     global currentFrame
     if currentFrame < numberOfFrames:
@@ -37,62 +39,100 @@ def read():
     return False, None
 
 
+def HomographyTest(p0, p1, frame):
+    pic = frame.copy()
+    H, status = cv2.findHomography(p0, p1, cv2.RANSAC, 3.0)
+    for (x0, y0), (x1, y1), good in zip(p0[:, 0], p1[:, 0], status[:, 0]):
+        if good:
+            cv2.line(pic, (x0, y0), (x1, y1), (0, 128, 0))
+        cv2.circle(pic, (x1, y1), 2, (red, green)[good], -1)
+    return pic, status
+
+
 # cam = cv2.VideoCapture("vosamples/videocutted.mp4")
 sift = cv2.xfeatures2d.SIFT_create()
 fast = cv2.FastFeatureDetector()
 
 ret = True
-stepcounter = 6
+stepcounter = 0
 track = []
 prevFrame = None
 p0 = None
+p1 = None
 prevGrayFrame = None
-while ret and stepcounter:
+prevStatus = None
+while ret and stepcounter < 100:
     # init
     if prevFrame is None:
         ret, prevFrame = read()
         if not ret:
             break
         prevGrayFrame = cv2.cvtColor(prevFrame, cv2.COLOR_BGR2GRAY)
-        p0 = cv2.goodFeaturesToTrack(prevGrayFrame, mask = None, **feature_params)
+        ret, curFrame = read()
+        if not ret:
+            break
+        curGrayFrame = cv2.cvtColor(curFrame, cv2.COLOR_BGR2GRAY)
     else:
-        prevFrame = frame
-        prevGrayFrame = grayFrame
-        p0 = p1
+        prevFrame = curFrame
+        curFrame = nextFrame
+        prevGrayFrame = curGrayFrame
+        curGrayFrame = nextGrayFrame
 
-    ret, frame = read()
+    ret, nextFrame = read()
     if not ret:
         break
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    p1 = cv2.goodFeaturesToTrack(prevGrayFrame, mask = None, **feature_params)
+    nextGrayFrame = cv2.cvtColor(nextFrame, cv2.COLOR_BGR2GRAY)
 
-    p2, trace_status = checkedTrace(prevGrayFrame, grayFrame, p1 )
+    p0 = cv2.goodFeaturesToTrack(prevGrayFrame, mask=None, **feature_params)
 
-    p1 = p1[trace_status].copy()
-    p2 = p2[trace_status].copy()
+    p1, trace_status = checkedTrace(prevGrayFrame, curGrayFrame, p0)
+    pic, prevStatus = HomographyTest(p0, p1, prevFrame)
+    bestA = (trace_status[:, ] >= 1) * (prevStatus[:, 0] >= 1)
 
-    H, status0 = cv2.findHomography(p1, p2, cv2.RANSAC, 3.0)
-    for (x0, y0), (x1, y1), good in zip(p1[:, 0], p2[:, 0], status0[:, 0]):
-        if good:
-            cv2.line(frame, (x0, y0), (x1, y1), (0, 128, 0))
-        cv2.circle(frame, (x1, y1), 2, (red, green)[good], -1)
+    p2, trace_status = checkedTrace(curGrayFrame, nextGrayFrame, p0)
+    pic, nextStatus = HomographyTest(p1, p0, curFrame)
+    bestB = (trace_status[:, ] >= 1) * (nextStatus[:, 0] >= 1)
 
-    cv2.imshow('lk_homography', frame)
-    cv2.waitKey(0)
+    bestC = bestA[:] * bestB[:]
 
+    a = p0[bestC].copy()
+    b = p1[bestC].copy()
+    c = p2[bestC].copy()
 
+    # cv2.imshow('lk_homography', pic)
+    # cv2.waitKey(0)
 
+    # counter = 1000
+    # flag = False
+    #
+    # while counter > 0 and not flag:
+    #     try:
+    #         choose_mask = np.random.choice(len(a), 9)
+    #         # flag, T, scale = process_vo(a[choose_mask, 0], b[choose_mask, 0], c[choose_mask, 0])
+    #         flag, T, scale = process_vo(a[:, 0], b[:, 0], c[:, 0])
+    #         track.append(np.array(T) / scale)
+    #         flag = True
+    #     except IOError as e:
+    #         counter = counter - 1
+
+    flag, T, scale = process_vo(a[:, 0], b[:, 0], c[:, 0])
+    track.append(np.array(T) / scale)
 
     stepcounter = 1 + stepcounter
 
+mplt = mPlot()
+t = np.array([0, 0, 0])
+print(track)
+for T in track:
+    print(T, t)
+    mplt.addVector(t + T, t)
+    t = t + T
+
+mplt.show()
+
+cam.release()
 
 exit(0)
-
-
-
-
-
-
 
 while ret and stepcounter:
     ret, frame = read()
